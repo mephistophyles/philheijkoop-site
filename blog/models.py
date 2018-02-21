0961c531747d2dc486dfd31d3ed3987dd2ec6769
Philip Heijkoop <mephistophyles@gmail.com>
2018-02-21 13:05:28 -0500

    created blog model and markdownify filter

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    originally_published_date = models.DateField(default=timezone.now())
+    published_date = models.DateField(blank=True, null=True)
+
+    def publish(self, date=None):
+        if date is None:
+            date = timezone.now()
+        self.published_date = date
+        self.save()
+
+    def __str__(self):
+        return self.title
