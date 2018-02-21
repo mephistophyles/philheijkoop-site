fa54edaf09f1dcf2bc9db0ea1e16ea8a074902e8
Philip Heijkoop <mephistophyles@gmail.com>
2018-02-21 12:46:52 -0500

    added a gitignore and .env file

--- philheijkoopsite/settings.py
+++ philheijkoopsite/settings.py
@@ -15,10 +15,10 @@ import os
 import dotenv
 
 
-dotenv.read_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv.read_dotenv(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
