e165920396e9c6d9eb4c4f9349b2ff8ed37a5f4d
Philip Heijkoop <mephistophyles@gmail.com>
2018-02-23 14:33:38 -0500

    added template to blogpost detailview and fixed markdownify filter

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
@@ -18,6 +18,7 @@ class BlogPostListView(ListView):
 class BlogPostDetailView(DetailView):
 # TODO we need to enable the correct context variable here to pass the information corresponding to the correct blog_post object, probably needs to be passed both here and in the urls (via the slug)
     model = BlogPost
+    template_name = 'blog/blogpost_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -41,7 +42,15 @@ def home(request):
 #
 # def blog(request):
 #     return render(request, 'templates/blog.html', {'blog_posts': BlogPost.objects.all()})
-
+#
+#
+# def blogPost(request, slug):
+#     try:
+#         blog_post = BlogPost.objects.get(slug=slug)
+#     except:
+#         raise Http404
+#     context = super()
+#     return render(request, 'templates/blogpost_detail.html', )
 
 def projects(request):
     return render(request, 'templates/projects.html', {})
