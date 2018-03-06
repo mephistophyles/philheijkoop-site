ec96c16054bb2393df07b0af7f72d0863f01fd79
Philip Heijkoop <mephistophyles@gmail.com>
2018-03-06 12:04:42 -0500

    added aws app location to wsgi.py

--- philheijkoopsite/wsgi.py
+++ philheijkoopsite/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, '/opt/python/current/app')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "philheijkoopsite.settings")
 
