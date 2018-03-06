27090872e8d2698ff2293a2f2a7987eddb5e143f
Philip Heijkoop <mephistophyles@gmail.com>
2018-03-06 15:02:55 -0500

    update settings file to remove dotenv for aws

--- philheijkoopsite/philheijkoopsite/settings.py
+++ philheijkoopsite/philheijkoopsite/settings.py
@@ -12,13 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-import dotenv
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-dotenv.read_dotenv(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -27,7 +23,11 @@ dotenv.read_dotenv(BASE_DIR)
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+if DEBUG:
+    import dotenv
+    dotenv.read_dotenv(BASE_DIR)
 
 ALLOWED_HOSTS = ["localhost",
                  'desolate-plains-37349.herokuapp.com',
